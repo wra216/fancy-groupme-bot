eae8561cb4f851790039ac3d7fad2b51b040c783
Wes <wellesleyarreza@ymail.com>
2015-03-24 09:25:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const events = require('events');
 const util = require('util');
 const http = require('http');
 const formidable = require('formidable');
+var express  = require('express');
+var expressapp=exports.app=express();
 
+console.log("Check that I can edit");
 // config { token:groupme token,
 //          group:the room to connect to,
 //          name: the bot name,
@@ -16,7 +19,7 @@ function Bot(config) {
   for (var key in config)
     if (config.hasOwnProperty(key)) this[key] = config[key];
   if (this.token && this.group && this.name) {
-    console.log("registering the bot");
+    console.log("registering the bot in the Bot(config) method");
     this.registerBot();
   } else {
     console.log("pass me config so you can actually be a bot");
@@ -25,11 +28,77 @@ function Bot(config) {
 
 util.inherits(Bot, events.EventEmitter);
 
+
+Bot.prototype.app=function(){
+  var self=this;
+  
+  expressapp.get('/',function(request,response){
+            response.writeHead(200, {
+        "Content-Type": "application/json"
+      });
+      response.end(JSON.stringify({
+        'name': self.name,
+        'group': self.group
+      }));
+});
+
+    expressapp.post('/incoming',function(request,response){
+    
+
+      var form = new formidable.IncomingForm();
+      var messageFields = {};
+      form.parse(request, function(err, fields, files) {
+        if (err) console.error("bad incoming data " + err);
+      });
+
+      form.on('field', function(name, value) {
+        messageFields[name] = value;
+      });
+
+      form.on('end', function() {
+        response.writeHead(200, {
+          "Content-Type": "text/plain"
+        });
+        response.end("THANKS");
+        if (typeof messageFields.payload !== 'undefined') {
+          self.emit('botImage', self, {
+            url: messageFields.payload.url,
+            picture_url: messageFields.payload.picture_url,
+            payload: messageFields.payload
+          });
+        } else {
+          self.emit('botMessage', self, {
+            attachments: messageFields.attachments,
+            avatar_url: messageFields.avatar_url,
+            created_at: messageFields.created_at,
+            group_id: messageFields.group_id,
+            id: messageFields.id,
+            name: messageFields.name,
+            sender_id: messageFields.sender_id,
+            sender_type: messageFields.sender_type,
+            source_guid: messageFields.source_guid,
+            system: messageFields.system,
+            text: messageFields.text,
+            user_id: messageFields.user_id,
+            payload: messageFields.payload
+          });
+        }
+      });
+
+
+  });
+
+
+};
+
+
 // start the web server
 // arg: address to serve on
 Bot.prototype.serve = function(address) {
   var self = this;
   var server = http.createServer(function(request, response) {
+    
+
     if (request.url == '/' && request.method == 'GET') {
       response.writeHead(200, {
         "Content-Type": "application/json"
@@ -38,7 +107,11 @@ Bot.prototype.serve = function(address) {
         'name': self.name,
         'group': self.group
       }));
-    } else if (request.url == '/incoming' && request.method == 'POST') {
+    } 
+
+
+
+    else if (request.url == '/incoming' && request.method == 'POST') {
       var form = new formidable.IncomingForm();
       var messageFields = {};
       form.parse(request, function(err, fields, files) {
@@ -88,7 +161,7 @@ Bot.prototype.serve = function(address) {
 
   }.bind(this));
 
-  server.listen(address);
+ // server.listen(address);
 };
 
 
@@ -155,6 +228,7 @@ Bot.prototype.serve2 = function() {
 
   //server.listen(address);   http://stackoverflow.com/questions/8514106/how-to-mount-express-js-sub-apps
 };
+
 
 
 
